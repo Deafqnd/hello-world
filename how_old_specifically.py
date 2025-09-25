15caddcd5b37988b42062fa0d0f5ef35420e6c97
Deafqnd <ayden.m.chan@gmail.com>
2025-09-25 09:26:54 -0400

    Create how_old_specifically.py
    modified version of how_old.py where there are more specific outputs for different age ranges

---
+++ how_old_specifically.py
@@ -0,0 +1,11 @@
+name = input('Hey man! What\'s your name? ')
+age = int(input(f'Ok, {name}, how old are you? '))
+
+if age <= 16:
+    print(f'You can\'t drive just yet, {name}.')
+elif age <= 17 and age >= 16:
+    print(f'You can drive, but you can\'t vote just yet, {name}.')
+elif age >= 18 and age <= 20:
+    print(f'You can vote now, but you can\'t drink just yet. Keep waiting!')
+else:
+    print(f'You can do pretty much anyhting, {name}, as long as it\'s legal!')
