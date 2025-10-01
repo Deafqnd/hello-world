12cfd1298a00401933382e37d71caac66c83e351
Deafqnd <ayden.m.chan@gmail.com>
2025-10-01 09:15:42 -0400

    Update and rename how_old_3.py to 034_age_compound_boolean.py

--- 034_age_compound_boolean.py
+++ 034_age_compound_boolean.py
@@ -2,10 +2,18 @@ name = input('Hey man! What\'s your name? ')
 age = int(input(f'Ok, {name}, how old are you? '))
 
 if age <= 16:
+    print(f'Your name: {name}')
+    print(f'Your age: {age}')
     print(f'You can\'t drive just yet, {name}.')
 if age <= 17 and age >= 16:
+    print(f'Your name: {name}')
+    print(f'Your age: {age}')
     print(f'You can drive, but you can\'t vote just yet, {name}.')
-if age >= 18 and age <= 20:
+if age >= 18 and age <= 24:
+    print(f'Your name: {name}')
+    print(f'Your age: {age}')
     print(f'You can vote now, but you can\'t drink just yet. Keep waiting!')
-if age >= 20:
+if age >= 25:
+    print(f'Your name: {name}')
+    print(f'Your age: {age}')
     print(f'You can do pretty much anyhting, {name}, as long as it\'s legal!')
